da9f21e0fc39c26cf5e179254e866a811fc07a35
Orta Therox <orta.therox@gmail.com>
2018-06-12 17:55:42 -0400

    Show logs

--- src/components/installation/Websocket.tsx
+++ src/components/installation/Websocket.tsx
@@ -1,12 +1,13 @@
 import * as React from "react"
-import { Button, Dropdown, Segment } from "semantic-ui-react"
+import { Button, Dropdown, Feed, Segment } from "semantic-ui-react"
+import relativeDate from "tiny-relative-date"
 
 interface Props {
   iID: string
 }
 
 interface State {
-  events: any[]
+  events: Notification[]
   connected: boolean
 }
 
@@ -21,23 +22,28 @@ export class Websocket extends React.Component<Props, State> {
   public componentDidMount() {
     // client side only
     console.log("Connecting to Peril's webserver")
-    const primus = new Primus(`https://staging-api.peril.systems?iID=${this.props.iID}`)
-    // const primus = new Primus(`${process.env.REACT_APP_PUBLIC_API_ROOT_URL}?iID=${this.props.iID}`)
+    // const primus = new Primus(`https://staging-api.peril.systems?iID=${this.props.iID}`)
+    const primus = new Primus(`${process.env.REACT_APP_PUBLIC_API_ROOT_URL}?iID=${this.props.iID}`)
 
-    primus.on("connection", spark => {
-      this.setState({ connected: true })
-      console.log("Connected")
+    // I do not understand why this doesn't work like I'd expect
 
-      spark.on("data", data => {
-        this.setState({ events: [...this.state.events, data] })
-      })
+    // primus.on("connection", spark => {
+    //   this.setState({ connected: true })
+    //   console.log("Connected")
 
-      spark.write({ foo: "bar" })
-    })
+    //   spark.on("data", data => {
+    //     this.setState({ events: [...this.state.events, data] })
+    //   })
 
-    primus.on("data", data => {
-      console.log("word:", data)
-      primus.write(data)
+    //   spark.write({ foo: "bar" })
+    // })
+
+    primus.on("data", (data: Notification) => {
+      const datedData = {
+        ...data,
+        date: new Date(),
+      }
+      this.setState({ connected: true, events: [...this.state.events, datedData] })
     })
 
     primus.on("disconnection", spark => {
@@ -49,14 +55,110 @@ export class Websocket extends React.Component<Props, State> {
     return (
       <Segment style={{ padding: "8em 0em" }} vertical>
         <p>{this.state.connected ? "Connected" : "Disconnected"}</p>
-        {this.state.events.map(e => (
-          <p>
-            <code>
-              <pre>{JSON.stringify(e, null, "  ")}</pre>
-            </code>
-          </p>
-        ))}
+
+        <Feed>
+          {this.state.events.map(e => {
+            switch (e.action) {
+              case "connected":
+                return connectedEvent(e)
+
+              case "started":
+                return dangerfileStartedEvent(e)
+
+              case "finished":
+                return dangerfileFinishedEvent(e)
+
+              case "log":
+                return dangerfileLogEvent(e)
+            }
+          })}
+        </Feed>
       </Segment>
     )
   }
 }
+
+// Taken from Peril
+
+type Notification = MSGDangerfileStarted | MSGDangerfileFinished | MSGDangerfileLog | MSGConnected
+
+interface MSGConnected {
+  action: "connected"
+  connected: boolean
+  date: Date
+}
+
+interface MSGDangerfileStarted {
+  action: "started"
+  event: string
+  filenames: string[]
+  date: Date
+}
+
+interface MSGDangerfileFinished {
+  action: "finished"
+  event: string
+  filenames: string[]
+
+  time: number
+  date: Date
+}
+
+interface MSGDangerfileLog {
+  action: "log"
+  event: string
+
+  filenames: string[]
+  log: string
+  date: Date
+}
+
+const connectedEvent = (event: MSGConnected) => (
+  <Feed.Event>
+    <Feed.Content>
+      <Feed.Summary>
+        Connected
+        <Feed.Date>{relativeDate(event.date)}</Feed.Date>
+      </Feed.Summary>
+    </Feed.Content>
+  </Feed.Event>
+)
+
+const dangerfileStartedEvent = (event: MSGDangerfileStarted) => (
+  <Feed.Event>
+    <Feed.Content>
+      <Feed.Summary>
+        <Feed.User>{event.event}</Feed.User> has started
+        <Feed.Date>{relativeDate(event.date)}</Feed.Date>
+      </Feed.Summary>
+      <Feed.Meta>
+        <Feed.Like>{event.filenames.join(", ")}</Feed.Like>
+      </Feed.Meta>
+    </Feed.Content>
+  </Feed.Event>
+)
+
+const dangerfileFinishedEvent = (event: MSGDangerfileFinished) => (
+  <Feed.Event>
+    <Feed.Content>
+      <Feed.Summary>
+        <Feed.User>{event.event}</Feed.User> has finished
+        <Feed.Date>{relativeDate(event.date)}</Feed.Date>
+      </Feed.Summary>
+    </Feed.Content>
+  </Feed.Event>
+)
+
+const dangerfileLogEvent = (event: MSGDangerfileLog) => (
+  <Feed.Event>
+    <Feed.Content>
+      <Feed.Summary>
+        Got Logs for <Feed.User>{event.event}</Feed.User>
+        <Feed.Date>{relativeDate(event.date)}</Feed.Date>
+        <Feed.Extra text>
+          <pre>{event.log}</pre>
+        </Feed.Extra>
+      </Feed.Summary>
+    </Feed.Content>
+  </Feed.Event>
+)
